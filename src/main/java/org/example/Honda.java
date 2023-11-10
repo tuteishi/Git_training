6e89d108ada9804a6c79a7fd35ee967213b01a82
Alexandr Sanets <123288852+tuteishi@users.noreply.github.com>
2023-11-10 21:04:44 +0300

    Update Honda.java

--- src/main/java/org/example/Honda.java
+++ src/main/java/org/example/Honda.java
@@ -4,5 +4,6 @@ public class Honda {
     public static void main( String[] args )
     {
         System.out.println( "Hello Honda!" );
+        System.out.println( "Honda go to commit B" );
     }
 }
